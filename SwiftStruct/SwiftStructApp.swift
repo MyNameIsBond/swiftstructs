dc2fe6dd74c828161f77c8bbf77f77149a315682
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-23 12:08:16 +0000

    Initial Commit

---
+++ SwiftStruct/SwiftStructApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStructApp.swift
+//  SwiftStruct
+//
+//  Created by Tony Chaidinis on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
